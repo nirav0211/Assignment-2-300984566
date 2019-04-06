c652100618f424066eb6228c52f5417477bff3b7
NIRAV PANDYA <npandy16@my.centennialcollege.ca>
2019-04-05 21:02:10 -0400

    Db URL Fixed Final

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,8 @@
 module.exports = {
-  "URI" : "mongodb://localhost/test",
+  //"URI" : "mongodb://localhost/test",
     //"URI" : "mongodb://thomas:a123456@ds060977.mlab.com:60977/comp308-w2019-lesson10b",
     
-    //"URI" : "mongodb://nirav:12345@assignment2-shard-00-00-ku48k.azure.mongodb.net:27017,assignment2-shard-00-01-ku48k.azure.mongodb.net:27017,assignment2-shard-00-02-ku48k.azure.mongodb.net:27017/test?ssl=true&replicaSet=Assignment2-shard-0&authSource=admin&retryWrites=true",
+    "URI" : "mongodb://nirav:12345@assignment2-shard-00-00-ku48k.azure.mongodb.net:27017,assignment2-shard-00-01-ku48k.azure.mongodb.net:27017,assignment2-shard-00-02-ku48k.azure.mongodb.net:27017/test?ssl=true&replicaSet=Assignment2-shard-0&authSource=admin&retryWrites=true",
     "secret": "SomeSecret"
 
 }
